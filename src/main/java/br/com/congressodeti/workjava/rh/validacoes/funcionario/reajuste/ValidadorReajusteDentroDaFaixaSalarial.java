6d72c1d0f08ef1c30727eb3c9e5253f65e2e2384
Rodrigo Caneppele <r.caneppele@gmail.com>
2018-04-14 10:12:37 -0300

    adiciona validador da faixa salarial do cargo em relacao ao reajuste

---
+++ src/main/java/br/com/congressodeti/workjava/rh/validacoes/funcionario/reajuste/ValidadorReajusteDentroDaFaixaSalarial.java
@@ -0,0 +1,22 @@
+package br.com.congressodeti.workjava.rh.validacoes.funcionario.reajuste;
+
+import java.math.BigDecimal;
+
+import br.com.congressodeti.workjava.rh.exceptions.BusinessException;
+import br.com.congressodeti.workjava.rh.models.Cargo;
+import br.com.congressodeti.workjava.rh.models.Reajuste;
+
+public class ValidadorReajusteDentroDaFaixaSalarial implements ValidadorReajuste {
+	
+	@Override
+	public void valida(Reajuste reajuste) {
+		BigDecimal salarioAtual = reajuste.getFuncionario().getSalario();
+		BigDecimal salarioReajustado = salarioAtual.add(reajuste.getValor());
+		Cargo cargo = reajuste.getFuncionario().getCargo();
+		
+		if (salarioReajustado.compareTo(cargo.getSalarioMaximo()) > 0) {
+			throw new BusinessException("Salario reajustado do funcionario nao pode ultrapassar o valor do salario maximo de seu cargo");
+		}
+	}
+
+}
